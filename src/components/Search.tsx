b4c76d3f39e1f7eb47bc5cbe70844fe49f5dca37
SOVETOV <sovotoffm@gmail.com>
2023-03-13 16:28:10 +0300

    настроил фильтр поля поиска

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 
 const Search: React.FC = () => {
+  const [value, setValue] = React.useState('');
+  const onClickClear = () => {
+    setValue('');
+  };
+  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value.replace(/[^0-9]/g, ''));
+  };
   return (
     <div className="flex justify-center mt-4">
       <div className="relative rounded-full">
         <input
-          type="number"
           className="w-full px-4 py-2 rounded-full text-gray-700 focus:outline-none border-2"
           placeholder="Поиск"
+          value={value}
+          onChange={onChangeInput}
+          maxLength={3}
         />
-        <button className="absolute top-0 right-0 h-full px-4 py-2">
+        <button onClick={onClickClear} className="absolute top-0 right-0 h-full px-4 py-2">
           <svg
             className="w-6 h-6 text-gray-700"
             fill="none"
